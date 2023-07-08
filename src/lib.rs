697522a7deffba81f1204fc9034d94b74af51215
Alen Šiljak <dev@alensiljak.eu.org>
2023-07-08 12:42:20 +0200

    struct definitions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
 /*!
  * Asset Allocation module
  */
+
+pub mod model;
+
+pub fn first() {
+
+}
+
+pub fn importTomlDefinition(definition: String) {
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{fs::{File, self}, io::BufReader};
+
+    use crate::importTomlDefinition;
+
+    fn test_import() {
+        let contents = fs::read_to_string("tests/allocation.toml")
+            .expect("AA file read");
+        
+        importTomlDefinition(contents);
+
+        // Assert
+
+    }
+}
---
+++ src/model.rs
@@ -0,0 +1,21 @@
+/*!
+ * Definitions of the structures
+ */
+
+pub struct AssetClassDefinition {
+    pub allocation: u8,
+    pub symbols: Vec<String>
+}
+
+pub struct AssetClass {
+  pub fullname: String,
+  pub allocation: u8,
+  pub allocated_value: u8,
+  pub current_allocation: u8,
+  pub current_value: u8,
+  pub diff: u8,
+  pub diff_amount: u8,
+  pub diff_perc: u8,
+  pub currency: String,
+  pub symbols: Vec<String>,
+}
