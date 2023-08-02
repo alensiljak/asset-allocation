c6d99ab547de9d16cae2963a1974bea60a5bf27d
Alen Šiljak <dev@alensiljak.eu.org>
2023-08-02 18:06:41 +0200

    correct iteration

--- src/lib.rs
+++ src/lib.rs
@@ -19,42 +19,44 @@ pub fn loadDefinition(definition: String) -> Vec<AssetClass> {
 
     // println!("{:?}", parsed);
 
-    let asset_classes = linearizeDefinition(allocation);
+    let asset_classes = linearizeDefinition(&allocation);
 
     // let result = await this.validateAndSave(assetClasses)
 
     todo!()
 }
 
-fn linearizeDefinition(definition: Table) {
+fn linearizeDefinition(root: &Table) -> Vec<AssetClass> {
     let mut result: Vec<AssetClass> = vec![];
 
-    println!("definition: {:?}", definition);
-
-    let allocation = definition.get("Allocation").unwrap().as_table().unwrap();
-    println!("allocation: {:?}", allocation);
-
-    for (name, property) in allocation {
-        if property.is_table() {
-            // todo: add to the list
-            // get_linear_asset_classes(property.as_table());
-        } else {
-            let mut ac = AssetClass::new();
-            ac.fullname = name.to_owned();
-            ac.allocation = property.as_integer().unwrap() as u8;
-
-            println!("added: {:?}", ac);
-
+    // process only the children.
+    for (key, value) in root {
+        if value.is_table() {
+            // add the root class
+            let ac = get_asset_class_from_toml(key, value);
             result.push(ac);
+
+            // add to the list
+            let children = linearizeDefinition(value.as_table().unwrap());
+            for child in children {
+                result.push(child);
+            }
         }
     }
+
+    result
 }
 
-fn get_linear_asset_classes(root: Table) {
-    // return only the children
-    for (key, value) in root {
-        println!("{:?} {:?}", key, value);
-    }
+fn get_asset_class_from_toml(name: &String, value: &Value) -> AssetClass {
+    let mut ac = AssetClass::new();
+    ac.fullname = name.to_owned();
+
+    // get the allocation
+    let node = &value["allocation"];
+    let node_value = node.as_integer().unwrap();
+    ac.allocation = node_value as u8;
+
+    ac
 }
 
 #[cfg(test)]
